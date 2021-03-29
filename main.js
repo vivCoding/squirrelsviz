9ffc24d8e11ebc521de6432bc631fcc12458db6d
vvvuPurdue <vvvu@purdue.edu>
2021-03-29 13:48:07 -0400

    speedrun

--- main.js
+++ main.js
@@ -12,27 +12,59 @@ let maxY = 40.8001185184892;
 let diffX = 0.03143681330610093;
 let diffY = 0.03520785077540012;
 
+// all unique dates for squirrel sightings
 let dates = ["10062018", "10072018", "10082018", "10102018", "10122018", "10132018", "10142018", "10172018", "10182018", "10192018", "10202018"];
+let ages = [undefined, 'Adult', 'Juvenile', '?'];
+
+let filters = {
+    dates: dates,
+    furColor: "all",
+    age: ages
+}
+
+// get some html elements
+let dateSelector = $("#dateSelector");
+let dateLabel = $("#dateLabel");
+let showAllDates = $("#showAllDates");
 
 // fetch that data, make sure we get all of it by settgin limit to 3500
-fetch("https://data.cityofnewyork.us/resource/vfnx-vebw.json?$limit=3500").then(response => {
-    return response.json()
-}).then(response => {
-    console.log("getting data", response.length);
-    squirrels = response;
-    drawSquirrels();
+$("document").ready(function() {
+    fetch("https://data.cityofnewyork.us/resource/vfnx-vebw.json?$limit=3500").then(response => {
+        return response.json()
+    }).then(response => {
+        console.log("getting data", response.length);
+        squirrels = response;
+        drawSquirrels();
+    })
 })
 
 // draw squirrels yippee
-function drawSquirrels(includeDates=dates) {
+function drawSquirrels() {
     console.log("drawing squirrels");
+    clearCanvas();
+    // handle bad date
+    if (filters.dates.length == 1 && !dates.includes(filters.dates[0])) {
+        ctx.font = "15px Arial";
+        ctx.fillStyle = "black";
+        ctx.fillText("no data found for specified date", canvas.width / 3, canvas.height / 3);
+        console.log("no data");
+    }
     squirrels.forEach(squirrel => {
-        if (includeDates.includes(squirrel.date)) {
+        if (filters.dates.includes(squirrel.date) &&
+            (filters.furColor == "all" || filters.furColor == squirrel.primary_fur_color) &&
+            filters.age.includes(squirrel.age)
+        ) {
             drawSquirrel(squirrel);
         }
     });
 }
 
+function clearCanvas() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+// wtf why am i abstracting stuff so much
 function drawSquirrel(squirrel) {
     let newCoords = convertCoord(squirrel.x, squirrel.y);
     if (squirrel.primary_fur_color == undefined) {
@@ -60,4 +92,36 @@ function drawDot(x, y, color, border="rgba(0,0,0,0)", size = 5) {
     ctx.lineWidth = 2;
     ctx.strokeStyle = border;
     ctx.strokeRect(x - size / 2, y - size / 2, size, size);
+}
+
+// handle input for dates
+function dateSelectorHandler(value="All") {
+    if (value == "All") {
+        filters.dates = dates;
+        dateLabel.text("All");
+        drawSquirrels();
+    } else {
+        showAllDates.prop("checked", false);
+        dateLabel.text(value.slice(0, 2) + "/" + value.slice(2, 4) + "/" + value.slice(4));
+        filters.dates = [value]
+        drawSquirrels();
+    }
+}
+
+// handle input for color
+function furColorHandler(value) {
+    filters.furColor = value == "unknown" ? undefined : value;
+    drawSquirrels();
+}
+
+// handle age
+function ageHandler(value) {
+    if (value == "?") {
+        filters.age = [undefined, "?"];
+    } else if (value == "all") {
+        filters.age = ages;
+    } else {
+        filters.age = [value];
+    }
+    drawSquirrels();
 }
